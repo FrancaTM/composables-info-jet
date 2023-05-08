aac6adabe6db422e3c5e5502bb5a235fc81cdd50
FrancaTM <francatm@outlook.com>
2023-05-08 13:22:31 -0300

    add app content finish layout

--- app/src/main/java/com/example/composablesinfo/MainActivity.kt
+++ app/src/main/java/com/example/composablesinfo/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composablesinfo.ui.theme.ComposablesInfoTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,20 +51,42 @@ fun ComposablesInfoApp() {
                 .fillMaxSize()
         ) {
             Column(
-                Modifier
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
                     .weight(0.5F, true)
                     .background(color = Color(0xFFEADDFF))
                     .fillMaxSize()
+                    .padding(16.dp)
             ) {
-
+                Text(
+                    text = stringResource(R.string.text_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.text_composable_content),
+                    textAlign = TextAlign.Justify
+                )
             }
             Column(
-                Modifier
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
                     .weight(0.5F, true)
                     .background(color = Color(0xFFD0BCFF))
                     .fillMaxSize()
+                    .padding(16.dp)
             ) {
-
+                Text(
+                    text = stringResource(R.string.image_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.image_composable_content),
+                    textAlign = TextAlign.Justify
+                )
             }
         }
         Row(
@@ -65,20 +95,42 @@ fun ComposablesInfoApp() {
                 .fillMaxSize()
         ) {
             Column(
-                Modifier
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
                     .weight(0.5F, true)
                     .background(color = Color(0xFFB69DF8))
                     .fillMaxSize()
+                    .padding(16.dp)
             ) {
-
+                Text(
+                    text = stringResource(R.string.row_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.row_composable_content),
+                    textAlign = TextAlign.Justify
+                )
             }
             Column(
-                Modifier
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
                     .weight(0.5F, true)
                     .background(color = Color(0xFFF6EDFF))
                     .fillMaxSize()
+                    .padding(16.dp)
             ) {
-
+                Text(
+                    text = stringResource(R.string.column_composable_title),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.column_composable_content),
+                    textAlign = TextAlign.Justify
+                )
             }
         }
     }
